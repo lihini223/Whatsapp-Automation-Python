661078d4f7bbfa9e89de70765bc1ed76cc2f3e93
Lihini Nisansala <lihininisansala95@gmail.com>
2021-02-23 23:21:57 +0530

    create function send_message

--- message.py
+++ message.py
@@ -1,1 +1,15 @@
+from twilio.rest import Client
+
+account_sid = 'AC89d03492e78f7cf541223bf89a7bcacd'
+auth_token = '6f7645afcea72ca7db3e0c02ea029121'
+client = Client(account_sid, auth_token)
+
+def send_message():
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body='I need you so badly.❤',
+        to='whatsapp:+0000000000'
+    )
+
+    print(message.sid)
 
