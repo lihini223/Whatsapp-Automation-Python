2a69b5e6683f4f4457a7e4a9b5a01aa094c12604
Lihini Nisansala <lihininisansala95@gmail.com>
2021-02-23 23:23:38 +0530

    Add scheduling function

--- clock.py
+++ clock.py
@@ -1,1 +1,12 @@
+from datetime import datetime
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from honey_love import send_message
+
+sched = BlockingScheduler()
+
+# Schedule job_function to be called every two hours
+sched.add_job(send_message, 'interval', seconds=10)
+
+sched.start()
